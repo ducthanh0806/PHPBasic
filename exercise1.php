15ecfdd6216a63e66bebbe43a526096ce6aab063
Nguyen Duc Thanh <ducthah6868@gmail.com>
2022-03-23 17:11:13 +0700

    fix php file 2nd time

--- exercise1.php
+++ exercise1.php
@@ -1,37 +1,39 @@
 <?php
-    function checkValidString($str) 
+    function checkValidString($str, $str1, $str2) 
     {
-        $str1 = "book";
-        $str2 = "restaurant";
-        if (((strstr($str, $str1) !== false) && (strstr($str, $str2) === false)) || 
-        ((strstr($str, $str1) === false) && (strstr($str, $str2) !== false))) {
+        if (((strstr($str, $str1) !== false) && (strstr($str, $str2) === false))
+           || ((strstr($str, $str1) === false) && (strstr($str, $str2) !== false))
+        ) {
             return true;
         }
         return false;
      }
 
+    $str1 = "book";
+    $str2 = "restaurant";
+    
     //Bai 1
-    var_dump(checkValidString("What an awful day"));    //return false
+    var_dump(checkValidString("What an awful day", $str1, $str2));    //return false
     echo "</br>";
-    var_dump(checkValidString("Turn right over here. The bookstore is on your left"));    //return true
+    var_dump(checkValidString("Turn right over here. The bookstore is on your left", $str1, $str2));    //return true
     echo "</br>";
-    var_dump(checkValidString("You can't fire me. I own these restaurants"));    //return true
+    var_dump(checkValidString("You can't fire me. I own these restaurants", $str1, $str2));    //return true
     echo "</br>";
-    var_dump(checkValidString("I have dinner in restaurant. After that go to bookstore"));    // return false
+    var_dump(checkValidString("I have dinner in restaurant. After that go to bookstore", $str1, $str2));    // return false
     echo "</br>";
 
     //Bai 2
-    function checkString($file) 
+    function checkString($file, $str1, $str2) 
     {
         $str = file_get_contents($file);
-        if (checkValidString($str)) {
+        if (checkValidString($str, $str1, $str2)) {
             echo "Chuỗi hợp lệ. Chuỗi bao gồm " . substr_count($str, ".") . " câu";
         } else {
             echo "Chuỗi không hợp lệ";
         }
     }
     echo "</br>";
-    checkString("file1.txt");    //Chuỗi hợp lệ. Chuỗi bao gồm 4 câu
+    checkString("file1.txt", $str1, $str2);    //Chuỗi hợp lệ. Chuỗi bao gồm 4 câu
     echo "</br>";
-    checkString("file2.txt");    //Chuỗi không hợp lệ
+    checkString("file2.txt", $str1, $str2);    //Chuỗi không hợp lệ
     
